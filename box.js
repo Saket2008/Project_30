5376c7a3e7f745b742de55209a55274618869476
Saket2008 <74353880+Saket2008@users.noreply.github.com>
2021-03-15 12:00:36 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,32 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.1,
+            'friction':0.3
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibility = 255;
+        this.image = loadImage("rectangle.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        if (this.body.speed < 5 ){
+          push()
+          //rotate(angle);
+          rectMode(CENTER);
+          image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+          pop()
+        }
+        else{
+          World.remove(world, this.body);
+          push();
+          this.visibility = this.visibility - 20;
+          tint(255, this.visibility);
+          image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+          pop();
+        }
+      }
+}
